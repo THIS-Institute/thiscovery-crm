c9027efc793d7444d3df45b5e2d14dc8e3b37078
André Sartori <afs25@cam.ac.uk>
2022-03-11 14:55:37 +0000

    #4361 comment: attempting deployment to test env

---
+++ .github/workflows/deploy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from thiscovery_dev_tools.aws_deployer import AwsDeployer
+from src.common.constants import STACK_NAME
+
+
+if __name__ == '__main__':
+    deployer = AwsDeployer(stack_name=STACK_NAME)
+    deployer.main(build_in_container=True, skip_confirmation=True)
