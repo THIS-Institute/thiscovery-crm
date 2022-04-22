825b9a03b38ac07032d281c8d3be3c45ecd4bc57
André Sartori <afs25@cam.ac.uk>
2022-04-22 11:49:04 +0100

    #4361 comment: Added debug step to log parsed template.yaml

--- actions_deploy.py
+++ actions_deploy.py
@@ -3,6 +3,10 @@ from thiscovery_dev_tools.aws_deployer import AwsDeployer
 from src.common.constants import STACK_NAME
 
 
-if __name__ == '__main__':
+def main():
     deployer = AwsDeployer(stack_name=STACK_NAME)
     deployer.main(build_in_container=True, skip_confirmation=True, skip_slack_notification=True)
+
+
+if __name__ == '__main__':
+    main()
