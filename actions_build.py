8c78faf3c6a4a6ff07a00578e087a42cbe772aee
André Sartori <afs25@cam.ac.uk>
2022-04-25 15:48:55 +0100

    #4533 comment: Attempt to use build and packaging in release.yaml workflow

--- actions_build.py
+++ actions_build.py
@@ -5,7 +5,9 @@ from src.common.constants import STACK_NAME
 
 def main():
     deployer = AwsDeployer(stack_name=STACK_NAME)
-    deployer.deploy()
+    deployer.parse_sam_template()
+    deployer.validate_template()
+    deployer.build(build_in_container=True)
 
 
 if __name__ == '__main__':
