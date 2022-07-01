47e0a5a6607d6a9163de7ffb829cf1300eb793b4
André Sartori <afs25@cam.ac.uk>
2022-07-01 16:42:52 +0100

    #4636 comment: moved HubSpot-related admin scripts from thiscovery-core

---
+++ local/admin_tasks/hubspot_admin/create_timeline_event_type_for_task_signup.py
@@ -0,0 +1,46 @@
+import thiscovery_lib.dynamodb_utilities as ddb
+import thiscovery_lib.hubspot_utilities as hs
+from thiscovery_lib.utilities import get_aws_namespace
+import api.local.dev_config
+import api.local.secrets
+
+
+def save_timeline_event_type_id(name: str, hubspot_id, correlation_id):
+    table_id = get_aws_namespace() + name
+    details = {
+        "hubspot_id": str(hubspot_id),
+        "name": str(name),
+    }
+    ddb_client = ddb.Dynamodb()
+    ddb_client.put_item("lookups", table_id, "tle_type", details, {}, True, correlation_id)
+
+
+def main():
+    hs_client = hs.HubSpotClient()
+
+    type_defn = {
+        "name": hs.TASK_SIGNUP_TLE_TYPE_NAME,
+        "objectType": "CONTACT",
+        "headerTemplate": "{{signup_event_type}} for {{task_name}}",
+        "detailTemplate": "Project: {{project_name}}  {{project_id}}\nTask type: {{task_type_name}}  {{task_type_id}}",
+    }
+
+    tle_type_id = hs_client.create_timeline_event_type(type_defn)
+
+    properties = [
+        {"name": "project_id", "label": "Project Id", "propertyType": "String"},
+        {"name": "project_name", "label": "Project Name", "propertyType": "String"},
+        {"name": "task_id", "label": "Task Id", "propertyType": "String"},
+        {"name": "task_name", "label": "Task Name", "propertyType": "String"},
+        {"name": "task_type_id", "label": "Task Type Id", "propertyType": "String"},
+        {"name": "task_type_name", "label": "Task Type", "propertyType": "String"},
+        {"name": "signup_event_type", "label": "Event Type", "propertyType": "String"},
+    ]
+
+    hs_client.create_timeline_event_type_properties(tle_type_id, properties)
+
+    save_timeline_event_type_id(hs.TASK_SIGNUP_TLE_TYPE_NAME, tle_type_id, None)
+
+
+if __name__ == "__main__":
+    main()
---
+++ local/admin_tasks/hubspot_admin/renew_hubspot_token.py
@@ -0,0 +1,20 @@
+from thiscovery_lib.hubspot_utilities import HubSpotClient, SingleSendClient
+
+
+def renew_token(client_class):
+    hs_client = client_class()
+    hs_client.get_new_token_from_hubspot()
+    print("New token:")
+    print(hs_client.access_token)
+
+
+if __name__ == "__main__":
+    target_app = ""
+    while target_app.lower() not in ["d", "e"]:
+        target_app = input(
+            "What HubSpot app are you renewing the token for?\nd = default (thiscovery)\ne = transactional emails (thiscovery emails)"
+        )
+    if target_app == "d":
+        renew_token(HubSpotClient)
+    elif target_app == "e":
+        renew_token(SingleSendClient)
