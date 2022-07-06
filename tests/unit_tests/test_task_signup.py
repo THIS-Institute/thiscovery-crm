279fdf02fa6ec89b8c5aaa480d3ced55d2129b72
André Sartori <afs25@cam.ac.uk>
2022-07-06 22:56:56 +0100

    #4635 comment: task signup notification processing now queries core API for crm id if not present in trigger event

--- tests/unit_tests/test_task_signup.py
+++ tests/unit_tests/test_task_signup.py
@@ -21,8 +21,10 @@ try:
 except ModuleNotFoundError:
     pass
 
+import copy
 import thiscovery_dev_tools.testing_tools as test_tools
 from http import HTTPStatus
+from thiscovery_lib.core_api_utilities import CoreApiClient
 from thiscovery_lib.hubspot_utilities import HubSpotClient, TASK_SIGNUP_TLE_TYPE_NAME
 
 import src.common.constants as const
@@ -40,7 +42,7 @@ test_user_task_dict = {
     "id": "9620089b-e9a4-46fd-bb78-091c8449d777",
     "created": "2018-06-13 14:15:16.171819+00",
     "modified": "2018-06-13 14:15:16.171819+00",
-    "user_id": "48e30e54-b4fc-4303-963f-2943dda2b139",
+    "user_id": "dceac123-03a7-4e29-ab5a-739e347b374d",
     "user_project_id": "0fdacd2a-6276-485a-aca5-f276dde742e8",
     "project_task_id": "6cf2f34e-e73f-40b1-99a1-d06c1f24381a",
     "task_provider_name": "Cochrane",
@@ -83,6 +85,55 @@ class TestTaskSignup(test_tools.BaseTestCase):
         result = test_tools.test_eb_request_v2(
             local_method=record_task_signup_event,
             aws_eb_event=test_task_signup_event,
+            lambda_name="RecordTaskSignup",
+            aws_processing_delay=5,
+            stack_name=const.STACK_NAME,
+        )
+        self.assertEqual(HTTPStatus.NO_CONTENT, result["statusCode"])
+        notification = test_utils.get_expected_notification(ut_id)
+        self.assertIsNotNone(notification)
+
+        # process notification
+        process_notifications(event=None, context=None)
+
+        # check user now has sign-up timeline event
+        hs_client = HubSpotClient(stack_name=const.STACK_NAME)
+        tle_type_id = hs_client.get_timeline_event_type_id(
+            TASK_SIGNUP_TLE_TYPE_NAME, correlation_id=None
+        )
+        result = hs_client.get_timeline_event(tle_type_id, ut_id)
+        self.assertEqual(ut_id, result["id"])
+        notification_details = notification["details"]
+        self.assertEqual(notification_details["project_task_id"], result["task_id"])
+        self.assertEqual("CONTACT", result["objectType"])
+        self.assertEqual("PSFU-05-pub-act", result["project_name"])
+
+        # check that notification message has been processed
+        notification = test_utils.get_expected_notification(ut_id)
+        self.assertEqual(
+            NotificationStatus.PROCESSED.value,
+            notification[NotificationAttributes.STATUS.value],
+        )
+
+    def test_record_task_signup_event_no_crm_id(self):
+        # setup real crm id in core's rds db
+        hs_client = HubSpotClient(stack_name=const.STACK_NAME)
+        response = hs_client.get_hubspot_contact_by_email("fred@email.co.uk")
+        test_user_crm_id = str(response["canonical-vid"])
+        user_jsonpatch = [
+            {"op": "replace", "path": "/crm_id", "value": str(test_user_crm_id)},
+        ]
+        core_client = CoreApiClient()
+        core_client.patch_user("dceac123-03a7-4e29-ab5a-739e347b374d", user_jsonpatch)
+
+        # create notification from event where crm_id is null (simulating registration not processed yet)
+        ut_id = "e7c7fcf8-023b-46ed-9e20-2d917cb86959"
+        test_event = copy.deepcopy(test_task_signup_event)
+        test_event["detail"]["extra_data"]["crm_id"] = None
+        test_event["detail"]["id"] = ut_id
+        result = test_tools.test_eb_request_v2(
+            local_method=record_task_signup_event,
+            aws_eb_event=test_event,
             lambda_name="RecordTaskSignup",
             aws_processing_delay=5,
             stack_name=const.STACK_NAME,
